cb4dbd919ca3b16f35bd7642ca75e1f76eaee83a
yangjian <809127232@qq.com>
2019-08-31 23:41:37 +0800

    提取出公共样式，完成导航栏与轮播图

--- web/src/main.js
+++ web/src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import './assets/scss/style.scss'
+import router from './router'
+
+// 使用vue-awesome-swiper实现轮播图
+import VueAwesomeSwiper from "vue-awesome-swiper"
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper)
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ web/src/views/Home.vue
@@ -0,0 +1,55 @@
+<template>
+    <!-- 首页 -->
+    <div class="home">
+        <swiper :options="swiperOption">
+            <swiper-slide>
+                <img class="w-100" src="../assets/images/lbt1.jpeg" />
+            </swiper-slide>
+            <swiper-slide>
+                <img class="w-100" src="../assets/images/lbt2.jpeg" />
+            </swiper-slide>
+            <swiper-slide>
+                <img class="w-100" src="../assets/images/lbt3.jpeg" />
+            </swiper-slide>
+            <div class="swiper-pagination pagination-home text-right px-3 pb-2" slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+    name: "home",
+    data() {
+        return {
+            swiperOption: {
+                pagination: {
+                    // 指定class
+                    el: ".pagination-home"
+                }
+            }
+        };
+    },
+    components: {
+        HelloWorld
+    }
+};
+</script>
+
+<style lang="scss">
+@import "../assets/scss/variables";
+
+.pagination-home {
+    .swiper-pagination-bullet {
+        opacity: 1;
+        border-radius: 0.1538rem;
+        background: map-get($colors, "white");
+
+        &.swiper-pagination-bullet-active {
+            background: map-get($colors, "info");
+        }
+    }
+}
+</style>
