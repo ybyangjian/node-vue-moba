876f656dbbc8701d5cf474e424f6110c19603551
yangjian <809127232@qq.com>
2019-09-05 10:49:22 +0800

    修改了上传url

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -86,7 +86,7 @@ module.exports = app => {
     app.post('/admin/api/upload', authMiddleware(), upload.single('file'), async (req, res) => {
 
         const file = req.file
-        file.url = `http://localhost:3000/uploads/${file.filename}`
+        file.url = `http://wz.ybyangj.cn/uploads/${file.filename}`
         res.send(file)
     })
 
